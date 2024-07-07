f1fe13e28f56451d908fa761d1a7d45ba317560e
tiffany <tbcasper16@gmail.com>
2024-07-07 16:23:03 -0500

    adding task zero - redis utils

---
+++ utils/redis.js
@@ -0,0 +1,52 @@
+import Redis from 'redis';
+
+class RedisClient {
+    constructor() {
+        this.client = Redis.createClient();
+        this.client.on('error', (err) => {
+            console.error('Redis client error:', err);
+        });
+    }
+    isAlive() {
+        return this.client.connected;
+    }
+
+    async get(key) {
+        return new Promise((resolve, reject) => {
+            this.client.get(key, (err, reply) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                resolve(reply);
+            });
+        });
+    }
+
+
+    async set(key, value, durationInSeconds) {
+        return new Promise((resolve, reject) => {
+            this.client.setex(key, durationInSeconds, value, (err, reply) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                resolve(reply);
+            });
+        });
+    }
+
+    async del(key) {
+        return new Promise((resolve, reject) => {
+            this.client.del(key, (err, reply) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                resolve(reply);
+            });
+        });
+    }
+}
+const redisClient = new RedisClient();
+export default redisClient;
