0ca04b0f1ec70c86cd7448ba00779789843348ef
tiffany <tbcasper16@gmail.com>
2024-07-07 16:58:05 -0500

    adding users and files and included database in url but still getting unhandled rejection error... moving on for now

--- utils/db.js
+++ utils/db.js
@@ -5,15 +5,18 @@ class DBClient {
         const host = process.env.DB_HOST || 'localhost';
         const port = process.env.DB_PORT || 27017;
         const database = process.env.DB_DATABASE || 'files_manager';
-        const url = `mongodb://${host}:${port}`;
+        const url = `mongodb://${host}:${port}/${database}`;
 
         this.client = new MongoClient(url, { useUnifiedTopology: true });
         this.client.connect();
         this.database = this.client.db(database);
+
+        this.users = this.database.collection('users');
+        this.files = this.database.collection('files');
     }
 
     isAlive() {
-        this.client.isConnected();
+        return this.client.isConnected();
     }
 
     async nbUsers() {
