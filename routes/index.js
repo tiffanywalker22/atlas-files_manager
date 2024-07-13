7aabbeeeb1275f1f13b7885877c258d2507c2a3b
Tiffany <tbcasper16@gmail.com>
2024-07-12 19:51:30 -0500

    made progress on files controller; still encountering an error but will circle back

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const AppController = require('../controllers/AppController');
 const UserController = require('../controllers/UsersController');
 const AuthController = require('../controllers/AuthController');
+const FilesController = require('../controllers/FilesController');
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
@@ -12,5 +13,9 @@ router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UserController.getMe);
 
 router.post('/users', UserController.postNew);
+
+router.post('/files', FilesController.postUpload);
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
 
 module.exports = router;
